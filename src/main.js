0023fa58f0d4d18ef789c3c974e9d058bafc3096
andy208 <1029123033@qq.com>
2020-07-01 16:57:32 +0800

    路由配置完成

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import 'amfe-flexible'
+import router from './router/index.js'
+import './style/common.less'
+import './style/iconfont.less'
+import { Button, Field } from 'vant'
 
 Vue.config.productionTip = false
+Vue.use(Button)
+Vue.use(Field)
 
 new Vue({
-  router,
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Login from '../views/Login.vue'
+import Register from '../views/Register'
+Vue.use(VueRouter)
+const router = new VueRouter({
+  routes: [
+    { path: '/login', component: Login },
+    { path: '/register', component: Register }
+  ]
+})
+export default router
